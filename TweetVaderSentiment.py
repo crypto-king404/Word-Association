bd95a3defd97ed54e708ced45f6533f760286bb7
therealrohan303 <rohandalal2004@gmail.com>
2024-07-20 14:08:36 -0400

    TweetVaderSentiment.py

--- TweetVaderSentiment.py
+++ TweetVaderSentiment.py
@@ -11,13 +11,10 @@ from nltk.sentiment.vader import SentimentIntensityAnalyzer
 import spacy
 import os
 
-# Initialize VADER sentiment analyzer
 sid = SentimentIntensityAnalyzer()
 
-# Load SpaCy English model
 nlp = spacy.load("en_core_web_sm")
 
-# Define the path to the CSV file on your desktop
 csv_file_path = os.path.expanduser('~/Desktop/EAAI25-main/training3.csv')
 data = pd.read_csv(csv_file_path)
 
@@ -31,9 +28,7 @@ def calculate_sentiment_from_parts_of_speech(nouns, adjectives):
 # Apply sentiment calculation for each row
 data['sentiment'] = data.apply(lambda row: calculate_sentiment_from_parts_of_speech(eval(row['nouns']), eval(row['adjectives'])), axis=1)
 
-# Display the updated DataFrame
 print(data.head())
 
-# Save the updated DataFrame to a new CSV file
 output_csv_file_path = os.path.expanduser('~/Desktop/EAAI25-main/training3_with_sentiment.csv')
 data.to_csv(output_csv_file_path, index=False)
